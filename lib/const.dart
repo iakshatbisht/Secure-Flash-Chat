cf5443f5e5bacfd3644efeba8b6f6af128baec30
Akshat Bisht <48102705+iakshatbisht@users.noreply.github.com>
2021-06-06 23:24:12 +0530

    Update const.dart

--- lib/const.dart
+++ lib/const.dart
@@ -9,9 +9,8 @@ class Constants{
   static final greyColor2 = Color(0xffE8E8E8);
   static final kPrimaryColor = Color(0xFF6F35A5);
   static final kPrimaryLightColor = Color(0xFFF1E6FF);
-  static final APP_ID = 'e18fd08182ea4b7d81657a13293d22ec';
-  static final twitterConsumerKey = 'mxZroN05rZRzkPcaXGo28c8Jp';
-  static final twitterConsumerSecret = '6pzegNsenhZKFxpyB1iYptwS5FPBMUNhesEKsDJXgX7LZUe5tb';
-  static final twitterCallbackURL = 'https://chats-530c6.firebaseapp.com/__/auth/handler';
-  static final PROJECT_SERVER_KEY = 'AAAAMgHdBAY:APA91bHYEKBsDB6eQNUdwWV7B6lIlulRfC5iHkGewHzBp-tYJ5aw4nwKhf0aSucs8sgPORo2R20Q9DXHlrpdaJs7LD-jMuEdMKzOyTFsgRsQGvz4GC0LPfA98eLirCpVYuJvkSFRHSPQ';
+  static final twitterConsumerKey = '<YOUR_TWITTER_CONSUMER_KEY>';
+  static final twitterConsumerSecret = '<YOUR_TWITTER_CONSUMER_SECRET>';
+  static final twitterCallbackURL = '<YOUR_FIREBASE_PROJECT_URL/CALLBACK_URL>';
+  static final PROJECT_SERVER_KEY = '<YOUR_FIREBASE_MESSAGING_KEY>';
 }
